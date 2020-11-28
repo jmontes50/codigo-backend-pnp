ad23bbb276254459365feaf201eaf1a58ec8fb66
Osmar Montesinos <osmarmontes50@gmail.com>
2020-11-28 10:08:35 -0500

    update

--- Semana4/playas_alternative.sql
+++ Semana4/playas_alternative.sql
@@ -1,25 +1,24 @@
 CREATE SCHEMA IF NOT EXISTS `playas` DEFAULT CHARACTER SET utf8 ;
 USE `playas` ;
 
-CREATE TABLE IF NOT EXISTS `playas_2`.`t_vehiculos` (
+CREATE TABLE IF NOT EXISTS `playas`.`t_vehiculos` (
   `veh_id` INT NOT NULL AUTO_INCREMENT,
   `veh_placa` VARCHAR(6) NULL,
   `veh_marca` VARCHAR(30) NULL,
   `veh_anio` YEAR NULL,
   `veh_modelo` VARCHAR(45) NULL,
   `veh_color` VARCHAR(20) NULL,
-  `t_vehiculoscol` VARCHAR(45) NULL,
   PRIMARY KEY (`veh_id`))
 ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `playas_2`.`t_playa` (
+CREATE TABLE IF NOT EXISTS `playas`.`t_playa` (
   `playa_id` INT NOT NULL AUTO_INCREMENT,
   `playa_dir` VARCHAR(100) NULL,
   `playa_cap` INT NULL,
   PRIMARY KEY (`playa_id`))
 ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `playas_2`.`t_registro` (
+CREATE TABLE IF NOT EXISTS `playas`.`t_registro` (
   `reg_id` INT NOT NULL AUTO_INCREMENT,
   `reg_fechin` TIMESTAMP NULL,
   `reg_fechfin` TIMESTAMP NULL,
@@ -30,12 +29,12 @@ CREATE TABLE IF NOT EXISTS `playas_2`.`t_registro` (
   INDEX `fk_t_registro_t_playa1_idx` (`playa_id` ASC) VISIBLE,
   CONSTRAINT `fk_t_registro_t_vehiculos`
     FOREIGN KEY (`veh_id`)
-    REFERENCES `playas_2`.`t_vehiculos` (`veh_id`)
+    REFERENCES `playas`.`t_vehiculos` (`veh_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_t_registro_t_playa1`
     FOREIGN KEY (`playa_id`)
-    REFERENCES `playas_2`.`t_playa` (`playa_id`)
+    REFERENCES `playas`.`t_playa` (`playa_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
