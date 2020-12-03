8e39cae433cde301812788776bd7fe546b039a2a
Osmar Montesinos <osmarmontes50@gmail.com>
2020-12-02 19:11:56 -0500

    update flaskbd

--- Semana4/08-flask-con-mysql.py
+++ Semana4/08-flask-con-mysql.py
@@ -15,6 +15,9 @@ app.config['MYSQL_USER']='root'
 app.config['MYSQL_PASSWORD']='root'
 app.config['MYSQL_DB']='supermercados'
 
+#En caso de tener problemas al ejecutar sentencias desde flask
+#ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password by 'root';
+
 #CREE UN VINCULO CON MI BD
 mysql = MySQL(app)
 
@@ -72,6 +75,81 @@ def agregar_super():
     ), 400
 
 #TAREA: Crear 1 ruta que permita agregar 1 cliente.
+#1. endpoint
+#2. recuperar la información del front (petición) JSON
+#3. tengo que validad que la información que estoy obteniendo es correcta
+#4. armar mi petición con sentencias SQL
+#5. dar una respuesta
+
+@app.route('/cliente/agregar', methods=['POST'])
+def agregar_cliente():
+  info = request.get_json()
+  if(info.__contains__('nombre') and info.__contains__('apellido') and info.__contains__('categoria')):
+    conexion = mysql.connection.cursor()
+    #En caso de que Uds solamente tengan que agregar un dato => conexion.execute('INSERT... VALUES(%s),(valor,))
+    conexion.execute('INSERT INTO t_cliente (nom_cli, ape_cli, cat_cli) VALUES (%s, %s, %s)', (info['nombre'],info['apellido'], info['categoria']))
+    mysql.connection.commit()
+    conexion.close()
+    return jsonify({
+      'message':'Cliente registrado con exito',
+      'content':info
+    }), 201
+  else:
+    return jsonify({
+      'message':'Faltan valores, verificar'
+    }), 400
+
+@app.route('/cliente/traer/<int:id>', methods=['GET'])
+def traercliente(id):
+  conexion = mysql.connection.cursor()
+  conexion.execute(f"SELECT * FROM t_cliente WHERE id_cli = {id}")
+  data = conexion.fetchone()
+  # fetchone() => trae la primera coincidencia
+  # fetchall() => trae todas las coincidencias
+  conexion.close()
+  # print(data)
+  if data:
+    return jsonify({
+      'id':data[0],
+      'nombre':data[1],
+      'apellido':data[2],
+      'categoria':data[3]
+    }), 200
+  return jsonify({
+    'message':'No existe el usuario con ese id'
+  }), 404
+
+@app.route('/clientesuper/agregar', methods=['POST'])
+def agregar_cliente_supermercado():
+  #INGRESAR UN CLIENTE CON SUPERMERCADO PERO PRIMERO QUE VALIDE SI EXISTE EL CLIENTE Y SI EXISTE EL SUPERMERCADO, CASO CONTRARIO QUE ME INDIQUE QUE FALTA ALGUNO DE LOS OTROS DOS 
+  data = request.get_json()
+  if(data.__contains__('id_cliente') and data.__contains__('id_super')):
+    conexion = mysql.connection.cursor()
+    conexion.execute(f"SELECT * FROM t_cliente WHERE id_cli = {data['id_cliente']}")
+    cliente = conexion.fetchone()
+    conexion.execute(f"SELECT * from t_supermercados WHERE id_super = {data['id_super']}")
+    supermercado = conexion.fetchone()
+    conexion.close()
+
+    if supermercado and cliente:
+      conexioninsert = mysql.connection.cursor()
+      conexioninsert.execute("INSERT INTO t_super_cli (id_cli, id_super) VALUES (%s, %s)", (data['id_cliente'],data['id_super']))
+      mysql.connection.commit()
+      conexioninsert.close()
+
+      #Ya todo validado y sacramentado madno una respuesta
+      return jsonify({
+        'message':'El cliente fue vinculado con el supermercado'
+      }),201
+    else:
+      return jsonify({
+        'message':'No existe el cliente o el supermercado, revisar por favor'
+      }),400
+  return jsonify({
+    'message':'Faltan datos en el body,revise el json enviado'
+  }),400
+
+#RETO: CREEN UNA BD Sencilla, repitan la creación de los endpoint
 
 if __name__ == '__main__':
   app.run(debug=True, port=8000)
