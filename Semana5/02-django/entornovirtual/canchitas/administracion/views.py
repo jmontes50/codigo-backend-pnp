387e9fc25f46a41bfa58ead48f75b204d9cb8921
Osmar Montesinos <osmarmontes50@gmail.com>
2020-12-14 19:02:26 -0500

    update

--- Semana5/02-django/entornovirtual/canchitas/administracion/views.py
+++ Semana5/02-django/entornovirtual/canchitas/administracion/views.py
@@ -12,5 +12,31 @@ class TipoCanchasView(ListCreateAPIView):
   def get(self, request):
     print(self.get_queryset())
     # return 'Ok'
-    respuesta = self.get_serializer(self.get_queryset())
-    return Response('Ok',status=status.HTTP_200_OK)
+    respuesta = self.get_serializer(self.get_queryset(), many=True)
+    print(respuesta.data)
+    # return Response('Ok',status=status.HTTP_200_OK)
+    return Response({
+      'ok':True,
+      'content':respuesta.data,
+      'message':None
+    })
+
+  def post(self, request):
+    #print(request.data)
+    respuesta = self.get_serializer(data=request.data)
+    print(respuesta.is_valid(raise_exception=True))
+    if respuesta.is_valid(raise_exception=False):
+      # con el save lo guardamos en la base de datos
+      respuesta.save()
+      return Response({
+        'ok':True,
+        'content':respuesta.data,
+        'message':None
+      }, status=status.HTTP_201_CREATED)
+    
+    else:
+      return Response({
+        'ok':False,
+        'content':None,
+        'message':'Hubo un error al crear el Tipo Cancha'
+      }, status=status.HTTP_400_BAD_REQUEST)
