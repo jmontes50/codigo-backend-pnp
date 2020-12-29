1bccba6596d6009509861fb5a55b17e94a645362
Osmar Montesinos <osmarmontes50@gmail.com>
2020-12-29 08:55:02 -0500

    update, refactorización

--- Semana7/ecommerce/controllers/Producto.js
+++ Semana7/ecommerce/controllers/Producto.js
@@ -1,23 +1,28 @@
 const {Producto, Categoria} = require('../config/Sequelize');
 
+//solamente va a ser una función más, no es un controlador
+const existeCategoria = (id) => {
+  return new Promise ((resolve, reject) => {
+    Categoria.findByPk(id)
+    .then(categoria => {
+      if(categoria){
+        resolve(true)
+      }else{
+        reject("No se encontro la categoria");
+      }
+    })
+  })
+}
+
 const crearProducto = (req, res) => {
   let {cat_id} = req.body;
 
-  Categoria.findByPk(cat_id)
-  .then(categoria => {
-    // Encontrando la categoria
-    if(categoria){
-      let cuerpo = req.body;
+  existeCategoria(cat_id)
+  .then(() => {
+    let cuerpo = req.body;
       let objProducto = Producto.build(cuerpo);
       // retornamos promesa
       return objProducto.save()
-    }else{
-      return res.status(404).json({
-        ok:false,
-        content:null,
-        message:'No se encontro la categoria'
-      });
-    }
   })//aqui capturamos el resolve de objProducto.save()
   .then(productoCreado => {
     return res.status(201).json({
@@ -27,10 +32,10 @@ const crearProducto = (req, res) => {
     })
   })
   .catch(error => {
-    res.status(500).json({
+    res.status(404).json({
       ok:false,
       content:error,
-      message:'Error al crear el producto'
+      message:'Error, verificar el content'
     })
   })
 }
@@ -88,6 +93,12 @@ const obtenerProductoPorId = (req, res) => {
       message:"Error interno"
     })
   })
+}
+
+const actualizarProducto = (req, res) => {
+  if(req.body.cat_id){ //Si es que tengo uan propiedad cat_id, dentro del body, buscare la categoria
+    
+  }
 }
 
 module.exports = {
