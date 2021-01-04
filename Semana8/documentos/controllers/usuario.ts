9bc05ef2144c10b55a59c96d1391db508b9fe4d5
Osmar Montesinos <osmarmontes50@gmail.com>
2021-01-04 12:46:49 -0500

    update

--- Semana8/documentos/controllers/usuario.ts
+++ Semana8/documentos/controllers/usuario.ts
@@ -1,7 +1,6 @@
 import {Request, Response} from 'express';
 import { Usuario } from "../config/mongoose";
 import { CallbackError } from "mongoose";
-import { usuarioSchema } from '../models/usuario';
 
 export let crearUsuario = (req:Request, res:Response) => {
   let objUsuario: any = new Usuario(req.body);
@@ -46,6 +45,51 @@ export let loginUsuario = (req:Request, res:Response) => {
       res.status(404).json({
         ok:false,
         message:'El usuario no existe'
+      })
+    }
+  })
+}
+
+export let devolverUsuarios = (req:Request, res:Response) => {
+  Usuario.find((error, usuarios) => {
+    if(error){
+      res.status(500).json({
+        ok:false,
+        content:error,
+        message:'Hubo un error al solicitar los usuarios'
+      })
+    }else{
+      res.json({
+        ok:true,
+        content:usuarios,
+        message:null
+      })
+    }
+  })
+}
+
+export let actualizarUsuarioPorId = (req:Request, res:Response) => {
+  let {id} = req.params;
+  //Podemos añadir varias opciones en nuestras operaciones, en este caso el new:true para que devuelve el registro actualzadoya que por defecto devolvera el previo, antes de actualizar
+  Usuario.findByIdAndUpdate(id, req.body, {new:true}, (error, usuarioActualizado:any) => {
+    if(req.body.password){
+      if(usuarioActualizado){
+        usuarioActualizado.encriptarPassword(req.body.password);
+        //acemos efectivo la actualización de usuario
+        usuarioActualizado.save();
+      }
+    }
+    if(error){
+      res.status(500).json({
+        ok:false,
+        content:error,
+        message:'Hubo un error al actualizar el usuario'
+      })
+    }else{
+      res.status(201).json({
+        ok:true,
+        content:usuarioActualizado,
+        message:'Se actualizo correctamente el usuario'
       })
     }
   })
--- Semana8/documentos/routes/Usuario.ts
+++ Semana8/documentos/routes/Usuario.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
-import { crearUsuario, loginUsuario } from "../controllers/usuario";
+import { crearUsuario, loginUsuario, devolverUsuarios, actualizarUsuarioPorId} from "../controllers/usuario";
 
 export let usuario_router =  Router();
 
 usuario_router.post('/registro', crearUsuario);
 usuario_router.post('/login',loginUsuario);
+usuario_router.get('/usuario',devolverUsuarios);
+usuario_router.put('/usuario/:id', actualizarUsuarioPorId);
