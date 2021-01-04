71c514673b9c3c1b1f5c1e3e7dcdd26519a875d9
Osmar Montesinos <osmarmontes50@gmail.com>
2021-01-04 15:17:27 -0500

    update

--- Semana8/documentos/controllers/emergencia.ts
+++ Semana8/documentos/controllers/emergencia.ts
@@ -1,20 +1,61 @@
-import {Request, Response} from 'express';
-import {Emergencia} from '../config/mongoose';
+import { Request, Response } from "express";
+import { Emergencia } from "../config/mongoose";
+// Tanto fs como path son modulos nativos de node.js y ya vienen instalados y listos para utilizar por defecto.
+//fs -> FileSystem, sirve para el manejo de archivos, insertar, eliminar, rellenar archivos dentro de mi proyecto.
+import fs from "fs";
+//path -> otra libreria para manejar de manera sencilla las rutas de mis archivos y carpetas
+import path from "path";
 
-export let crearEmergencia = (req: Request, res:Response) => {
-  Emergencia.create(req.body, function(error:any, emergenciaCreada:any){
-    if(error){
+export let crearEmergencia = (req: Request, res: Response) => {
+  Emergencia.create(req.body, function (error: any, emergenciaCreada: any) {
+    if (error) {
       res.status(500).json({
-        ok:false,
-        content:error,
-        message:'Hubo un error al registrar la emergencia'
-      })
-    }else{
+        ok: false,
+        content: error,
+        message: "Hubo un error al registrar la emergencia",
+      });
+    } else {
       res.status(201).json({
-        ok:true,
-        content:emergenciaCreada,
-        message:'Emergencia creada con éxito'
-      })
+        ok: true,
+        content: emergenciaCreada,
+        message: "Emergencia creada con éxito",
+      });
     }
-  })
-}
+  });
+};
+
+export let subirImagen = (req: Request, res: Response) => {
+  let { id } = req.params;
+  // console.log(req.files);
+  if (req.files.imagen) {
+    let ruta = req.files.imagen.path;
+    // console.log({ruta})
+    let nombreArchivo = ruta.split("\\")[1];
+    console.log(nombreArchivo);
+    Emergencia.findByIdAndUpdate(
+      id,
+      { eme_img: nombreArchivo },
+      { new: true },
+      (error, emergenciaActualizada) => {
+        if (!error) {
+          res.status(201).json({
+            ok: true,
+            content: emergenciaActualizada,
+            message: "emergencia actualizada",
+          });
+        } else {
+          res.status(500).json({
+            ok: false,
+            content: error,
+            message: "Hubo un error al subir el archivo",
+          });
+        }
+      }
+    );
+  } else {
+    res.status(400).json({
+      ok: false,
+      message: "Falta la imagen",
+    });
+  }
+};
--- Semana8/documentos/routes/Emergencia.ts
+++ Semana8/documentos/routes/Emergencia.ts
@@ -1,6 +1,9 @@
 import {Router} from 'express';
-import {crearEmergencia} from "../controllers/emergencia";
+import {crearEmergencia, subirImagen} from "../controllers/emergencia";
+let multiparty = require('connect-multiparty');
+let multipartyMiddleware = multiparty({uploadDir:'./multimedia'})
 
 export let emergencia_router = Router();
 
 emergencia_router.post('/emergencia', crearEmergencia);
+emergencia_router.post('/subirImg/:id',multipartyMiddleware ,subirImagen);
